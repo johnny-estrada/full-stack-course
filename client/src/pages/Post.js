c79104b245e0b3f6cecf017bd69ee036d518135e
Johnny Estrada <johnnye562@yahoo.com>
2023-01-26 12:51:49 -0800

    Add createPost functionality

--- client/src/pages/Post.js
+++ client/src/pages/Post.js
@@ -3,8 +3,8 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 
 function Post() {
-  let { id } = useParams();
   const [postObject, setPostObject] = useState({});
+  let { id } = useParams();
 
   useEffect(() => {
     axios.get(`http://localhost:5001/posts/byId/${id}`).then((response) => {
@@ -15,7 +15,17 @@ function Post() {
   })
 
   return (
-    <div>{postObject.title}</div>
+    <div className="postPage">
+      <div className="leftSide">
+        <div className="post" id="individual">
+          <div className="title">{postObject.title}</div>
+          <div className="body">{postObject.postText}</div>
+          <div className="footer">{postObject.username}</div>
+        </div>
+      </div>
+
+      <div className="rightSide">Comment Section</div>
+    </div>
   ) 
 }
 
