42f173596f33eed7c5de08e87a2cd416f9af893e
Johnny Estrada <johnnye562@yahoo.com>
2023-02-01 14:40:16 -0800

    Update server to move db connection

--- server/index.js
+++ server/index.js
@@ -1,24 +1,23 @@
 // Instance of the framework express
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const db = require('./models')
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const db = require("./models");
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 // Routers
-const postsRouter = require('./routes/posts')
-const commentsRouter = require('./routes/comments')
+const postsRouter = require("./routes/posts");
+const commentsRouter = require("./routes/comments");
 
-app.use('/posts', postsRouter )
-app.use('/comments', commentsRouter )
-
-
-db.sequelize.sync().then(() => {
-    console.log('Database connection successful...')
-})
+app.use("/posts", postsRouter);
+app.use("/comments", commentsRouter);
 
 app.listen(5001, () => {
-    console.log('Server is running on port 5001...')
-})
+  console.log("Server is running on port 5001...");
+
+  db.sequelize.sync().then(() => {
+    console.log("Database connection successful...");
+  });
+});
